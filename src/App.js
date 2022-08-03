acbf98b2f68c4ee909e79efce3964091be2f243b
pproud <chitchanok.lcr@gmail.com>
2022-08-03 10:52:45 +0700

    basic hook: useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
+import React, { useState, useEffect  } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
-function App() {
+const randomCat = () => axios.get('https://aws.random.cat/meow')
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [cat, setCat] = useState({})
+
+  useEffect(() => {
+    randomCat().then((response) => setCat(response.data))
+  }, [count]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <p>You random {count} cats</p>
+        <Button variant="contained"
+         
+          onClick={() => setCount(count + 1)}
         >
-          Learn React
-        </a>
+          Click me
+        </Button>
+
+        <p>
+        <img src={cat.file} alt="Cat" width="256" />
+      </p>
+
       </header>
     </div>
   );
-}
+};
 
 export default App;
